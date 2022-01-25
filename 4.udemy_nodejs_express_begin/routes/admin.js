d1f9452118924ab568bf69e1d151bd55604b08bb
asdamwongmantap <asdamgrimson@gmail.com>
2022-01-25 09:23:36 +0700

    add html page

--- 4.udemy_nodejs_express_begin/routes/admin.js
+++ 4.udemy_nodejs_express_begin/routes/admin.js
@@ -1,20 +1,18 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
+const rootDir = require('../util/path')
+
+// /admin/add-product => GET
 router.get('/add-product',(req,res,next) => {
     console.log('In another middleware');
-    res.send(`<html>
-    <body>
-        <form action="/product" method="POST">
-            <input type="text" name="title">
-            <button type="submit">Simpan
-        </form>
-    </body>
-    </html>`)
+    res.sendFile(path.join(rootDir, 'views','add-product.html'))
 })
 
-router.post('/product',(req,res,next) => {
+// /admin/add-product => POST
+router.post('/add-product',(req,res,next) => {
     console.log(req.body);
     res.redirect('/');
 })
